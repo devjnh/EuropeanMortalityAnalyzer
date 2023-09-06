722dd994e5cdb86329b5e8276fa935d91568f6f3
devjnh <142326699+devjnh@users.noreply.github.com>
2023-09-07 00:17:57 +0200

    Added MortalityEvolution

--- EuropeanMortalityAnalyzer/Program.cs
+++ EuropeanMortalityAnalyzer/Program.cs
@@ -1,14 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using EuropeanMortalityAnalyzer;
 using EuropeanMortalityAnalyzer.Parser;
+using EuropeanMortalityAnalyzer.Views;
+
 class Program
 {
     static int Main(string[] args)
     {
         string folder = Directory.GetCurrentDirectory();
         DatabaseEngine databaseEngine = GetDatabaseEngine(folder);
-        EuroStatWeekly euroStatWeekly = new EuroStatWeekly { DatabaseEngine = databaseEngine};
-        euroStatWeekly.Extract(folder);
+        //EuroStatWeekly euroStatWeekly = new EuroStatWeekly { DatabaseEngine = databaseEngine};
+        //euroStatWeekly.Extract(folder);
+        MortalityEvolution mortalityEvolution = new MortalityEvolution { DatabaseEngine = databaseEngine};
+        mortalityEvolution.TimeMode = TimeMode.Semester;
+        mortalityEvolution.MinAge = 5;
+        mortalityEvolution.MaxAge = 40;
+        mortalityEvolution.MinYearRegression = 2015;
+        //mortalityEvolution.Country = "FR";
+        mortalityEvolution.Generate();
+        MortalityEvolutionView mortalityEvolutionView = new MortalityEvolutionView { MortalityEvolution = mortalityEvolution};
+        mortalityEvolutionView.Save();
         return 0;
     }
     private static DatabaseEngine GetDatabaseEngine(string dataFolder)
