0edc04470396ddf94ffb8501b8e0de87481a6b02
devjnh <142326699+devjnh@users.noreply.github.com>
2023-10-14 17:44:55 +0200

    Use CsvParser for AgeStructure

--- EuropeanMortalityAnalyzer/Parser/AgeStructure.cs
+++ EuropeanMortalityAnalyzer/Parser/AgeStructure.cs
@@ -1,6 +1,7 @@
 ﻿using MortalityAnalyzer.Common.Model;
 using MortalityAnalyzer.Downloaders;
 using MortalityAnalyzer.Model;
+using MortalityAnalyzer.Parser;
 using OfficeOpenXml;
 using System;
 using System.Collections.Generic;
@@ -13,11 +14,10 @@ using System.Threading.Tasks;
 
 namespace MortalityAnalyzer
 {
-    class AgeStructure
+    class AgeStructure : CsvParser
     {
         static public int ReferenceYear { get; set; } = 2022;
         public DataTable DataTable { get; private set; }
-        public DatabaseEngine DatabaseEngine { get; set; }
         const string SourceName = "demo_pjan";
 
         static AgeStructure()
@@ -27,18 +27,22 @@ namespace MortalityAnalyzer
 
         public void Load(string baseFolder)
         {
-            DataTable = DatabaseEngine.CreateDataTable(typeof(AgeStatistic), "AgeStructure");
-            DataTable.PrimaryKey = new DataColumn[] { DataTable.Columns[nameof(AgeStatistic.Year)], DataTable.Columns[nameof(AgeStatistic.Age)], DataTable.Columns[nameof(AgeStatistic.Gender)], DataTable.Columns[nameof(AgeStatistic.Country)] };
-
-            Console.WriteLine($"Loading age structure");
             try
             {
-                DatabaseEngine.FillDataTable("SELECT Year, Age, Population, Gender, Country FROM AgeStructure ORDER BY Year, Age", DataTable);
-                Console.WriteLine($"Age structure loaded");
+                LoadAgeStructure();
                 return;
             }
             catch { }
             Extract(baseFolder);
+            LoadAgeStructure();
+        }
+
+        private void LoadAgeStructure()
+        {
+            DataTable = CreateDataTable();
+            Console.WriteLine($"Loading age structure");
+            DatabaseEngine.FillDataTable("SELECT Year, Age, Population, Gender, Country FROM AgeStructure ORDER BY Year, Age", DataTable);
+            Console.WriteLine($"Age structure loaded");
         }
 
         private void Extract(string baseFolder)
@@ -46,49 +50,38 @@ namespace MortalityAnalyzer
             EuroStatDownloader euroStatDownloader = new EuroStatDownloader(baseFolder);
             euroStatDownloader.Download(SourceName);
             Console.WriteLine($"Extracting age structure");
-            DatabaseEngine.Prepare(DataTable);
-
-            using (FileStream fileStream = new FileStream(Path.Combine(baseFolder, $"{SourceName}.csv"), FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
-            {
-                using (StreamReader textReader = new StreamReader(fileStream))
-                {
-                    string line = textReader.ReadLine();
-                    string[] header = line.Split(',');
-                    while (!textReader.EndOfStream)
-                    {
-                        line = textReader.ReadLine();
-                        string[] split = line.Split(',');
-                        string age = split[4];
-                        string sex = split[5];
-                        string geo = split[6];
-                        string period = split[7];
-                        string count = split[8];
-                        if (age == "TOTAL" || age == "UNK")
-                            continue;
-                        Regex regexAge = new Regex("Y([0-9]+)");
-                        var result = regexAge.Match(age);
-                        if (!result.Success)
-                            continue;
-                        GenderFilter gender = sex == "F" ? GenderFilter.Female : sex == "M" ? GenderFilter.Male : GenderFilter.All;
-                        AgeStatistic ageStatistic = new AgeStatistic();
-                        ageStatistic.Year = Convert.ToInt32(period);
-                        ageStatistic.Age = Convert.ToInt32(result.Groups[1].Value);
-                        ageStatistic.Population = String.IsNullOrWhiteSpace(count) ? 0 : Convert.ToInt32(count);
-                        ageStatistic.Gender = gender;
-                        ageStatistic.Country = geo;
-                        DatabaseEngine.Insert(ageStatistic);
-                        //Regex regexWeek = new Regex("([0-9]{4})-W([0-9]{2})");
-                        //var result = regexWeek.Match(period);
-                        //if (!result.Success)
-                        //    continue;
-                        //int year = Convert.ToInt32(result.Groups[1].Value);
-                        //int week = Convert.ToInt32(result.Groups[2].Value);
-                        //DateTime weekDate = FirstDateOfWeekISO8601(year, week);
-                    }
-                }
-            }
-            DatabaseEngine.FinishInsertion();
+            ImportFromCsvFile(Path.Combine(baseFolder, $"{SourceName}.csv"));
             Console.WriteLine($"Age structure inserted");
+        }
+
+        protected override object GetEntry(string[] split)
+        {
+            string age = split[4];
+            string sex = split[5];
+            string geo = split[6];
+            string period = split[7];
+            string count = split[8];
+            if (age == "TOTAL" || age == "UNK")
+                return null;
+            Regex regexAge = new Regex("Y([0-9]+)");
+            var result = regexAge.Match(age);
+            if (!result.Success)
+                return null;
+            GenderFilter gender = sex == "F" ? GenderFilter.Female : sex == "M" ? GenderFilter.Male : GenderFilter.All;
+            AgeStatistic ageStatistic = new AgeStatistic();
+            ageStatistic.Year = Convert.ToInt32(period);
+            ageStatistic.Age = Convert.ToInt32(result.Groups[1].Value);
+            ageStatistic.Population = String.IsNullOrWhiteSpace(count) ? 0 : Convert.ToInt32(count);
+            ageStatistic.Gender = gender;
+            ageStatistic.Country = geo;
+
+            return ageStatistic;
+        }
+        protected override DataTable CreateDataTable()
+        {
+            DataTable  dataTable = DatabaseEngine.CreateDataTable(typeof(AgeStatistic), "AgeStructure");
+            dataTable.PrimaryKey = new DataColumn[] { dataTable.Columns[nameof(AgeStatistic.Year)], dataTable.Columns[nameof(AgeStatistic.Age)], dataTable.Columns[nameof(AgeStatistic.Gender)], dataTable.Columns[nameof(AgeStatistic.Country)] };
+            return dataTable;
         }
 
         public int MaxAge { get { return 100; } }
@@ -97,14 +90,14 @@ namespace MortalityAnalyzer
         {
             int ageLowerBound = age <= MaxAge ? age : MaxAge;
             DataRow[] rows = DataTable.Select($"Year={year} AND Age={ageLowerBound} AND Gender={(int)genderFilter} AND Country = {country}");
-            return (int)rows[0][2];
+            return (int)rows[0][nameof(AgeStatistic.Population)];
         }
         public int GetPopulation(int year, int minAge, int maxAge, string country, GenderFilter genderFilter = GenderFilter.All)
         {
             if (year > 2022)
                 year = 2022;
             DataRow[] rows = DataTable.Select($"Year={year} AND Age>={minAge}  AND Age<{maxAge} AND Gender={(int)genderFilter} AND Country = '{country}'");
-            return rows.Sum(r => (int)rows[0][2]);
+            return rows.Sum(r => (int)rows[0][nameof(AgeStatistic.Population)]);
         }
     }
 }
