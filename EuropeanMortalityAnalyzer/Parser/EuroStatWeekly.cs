ad3bf06ad4a288e5ac4886d6de7469cef1463ae4
devjnh <142326699+devjnh@users.noreply.github.com>
2023-10-27 21:42:26 +0200

    Added gender filter for mortality

--- EuropeanMortalityAnalyzer/Parser/EuroStatWeekly.cs
+++ EuropeanMortalityAnalyzer/Parser/EuroStatWeekly.cs
@@ -38,7 +38,7 @@ namespace MortalityAnalyzer.Parser
             string geo = split[6];
             string period = split[7];
             string count = split[8];
-            if (sex != "T" || age == "TOTAL" || age == "UNK")
+            if (age == "TOTAL" || age == "UNK")
                 return null;
             Regex regexWeek = new Regex("([0-9]{4})-W([0-9]{2})");
             var result = regexWeek.Match(period);
@@ -53,11 +53,11 @@ namespace MortalityAnalyzer.Parser
                 return null;
             int minAge = Convert.ToInt32(result.Groups[1].Value);
             int maxAge = Convert.ToInt32(result.Groups[2].Value);
-            int deaths = Convert.ToInt32(count);
+            int deaths = string.IsNullOrWhiteSpace(count) ? 0 : Convert.ToInt32(count);
             DeathStatistic deathStatistic = new DeathStatistic();
             deathStatistic.Age = minAge;
             deathStatistic.AgeSpan = maxAge + 1 - minAge;
-            deathStatistic.Gender = GenderFilter.All;
+            deathStatistic.Gender = GetGender(sex);
             deathStatistic.Country = geo;
             deathStatistic.Date = weekDate.AddDays(3); // Use the date of the thursday (middle of the week)
             deathStatistic.Deaths = deaths;
@@ -66,6 +66,17 @@ namespace MortalityAnalyzer.Parser
             deathStatistic.StandardizedDeaths = (double)deathStatistic.Deaths * deathStatistic.RefPopulation / deathStatistic.Population;
 
             return deathStatistic;
+        }
+
+        private static GenderFilter GetGender(string sex)
+        {
+            switch (sex)
+            {
+                case "T": return GenderFilter.All;
+                case "M": return GenderFilter.Male;
+                case "F": return GenderFilter.Female;
+            }
+            throw new ArgumentException("Unsuported gender");
         }
 
         public bool IsBuilt => DatabaseEngine.DoesTableExist(typeof(DeathStatistic));
